b38396fbcd686d1fe24ded73b9b5fc028ba52170
Sam Genduso <sam.genduso@gmail.com>
2015-07-21 15:59:57 -0600

    jasmine linked, first test passes

--- product-of-array-except-at-index/spec/spec.js
+++ product-of-array-except-at-index/spec/spec.js
@@ -1,11 +1,11 @@
-var products = require('solution.products');
+var myCode = require('../solution.js');
 
 describe('products', function() {
 
   it('takes an array and for each index of the array returns the product of every integer except at that index', function() {
     var integers = [1, 7, 3, 4];
 
-    expect(products(integers)).toEqual([84, 12, 28, 21]);
+    expect(myCode.products(integers)).toEqual([84, 12, 28, 21]);
   });
 
 });
