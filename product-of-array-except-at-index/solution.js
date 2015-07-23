b361091cceb9a2efe96c2f8e721445fb3540d31c
Sam Genduso <sam.genduso@gmail.com>
2015-07-22 20:41:55 -0600

    works with zeros in array

--- product-of-array-except-at-index/solution.js
+++ product-of-array-except-at-index/solution.js
@@ -1,25 +1,11 @@
-// You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
-//
-// For example, given:
-//
-//   [1, 7, 3, 4]
-// your function would return:
-//
-//   [84, 12, 28, 21]
-// by calculating:
-//
-//   [7*3*4, 1*3*4, 1*7*4, 1*7*3]
-// Do not use division in your solution.
-
-
 
 var products = function (arr) {
   var output = [];
   for (var i = 0; i < arr.length; i++) {
-    var productOfOthers = 0;
+    var productOfOthers = 'start';
     for (var j = 0; j < arr.length; j++) {
       if (j!==i) {
-       if (productOfOthers === 0) {
+       if (productOfOthers === 'start') {
           productOfOthers = arr[j];
         } else {
         productOfOthers *= arr[j];
@@ -31,10 +17,6 @@ var products = function (arr) {
   }
   return output;
 };
-
-var questionArray = [1, 7, 3, 4];
-
-console.log(products(questionArray));
 
 module.exports = {
   products:products
--- product-of-array-except-at-index/spec/spec.js
+++ product-of-array-except-at-index/spec/spec.js
@@ -4,8 +4,11 @@ describe('products', function() {
 
   it('takes an array and for each index of the array returns the product of every integer except at that index', function() {
     var integers = [1, 7, 3, 4];
+    var intsWithZeros = [1, 0, 7, 3, 4];
 
     expect(myCode.products(integers)).toEqual([84, 12, 28, 21]);
+
+    expect(myCode.products(intsWithZeros)).toEqual([0, 84, 0, 0, 0]);
   });
 
 });
